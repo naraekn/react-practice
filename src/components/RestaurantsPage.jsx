8df2b39f73fde837747e85a2c77409922639d12e
naraekn <34229459+naraekn@users.noreply.github.com>
2020-09-07 23:08:59 +0900

    Use axios to fetch regions
    Fetch regions data with axios.

--- src/components/RestaurantsPage.jsx
+++ src/components/RestaurantsPage.jsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
+
 export default function RestarauntsPage() {
   const [regions, setRegions] = useState([]);
 
   async function fetchRegions() {
-    const url = 'https://eatgo-customer-api.ahastudio.com/regions';
-    const response = await fetch(url);
-    const data = await response.json();
-    setRegions(data);
+    try {
+      const request = await axios
+        .get('https://eatgo-customer-api.ahastudio.com/regions');
+
+      setRegions(request.data);
+    } catch(error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
